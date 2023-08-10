1b54effeb6bba8b0de4a376d88d4990b1283e52a
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-08-10 08:17:57 -0400

    identity is real

--- SegmentSniperApi/Helpers/WebApplicationBuilderConfig.cs
+++ SegmentSniperApi/Helpers/WebApplicationBuilderConfig.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using SegmentSniper.Data;
