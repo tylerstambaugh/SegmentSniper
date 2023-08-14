5b229d34699e8e401c9fe88ecf726ae010d7f417
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-08-13 20:28:40 -0400

    unit test starts now

--- SegmentSniper.Data/ISegmentSniperDbContext.cs
+++ SegmentSniper.Data/ISegmentSniperDbContext.cs
@@ -5,8 +5,10 @@ namespace SegmentSniper.Data
 {
     public interface ISegmentSniperDbContext
     {
-        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+       // DbSet<TEntity> Set<TEntity>() where TEntity : class;
 
         DbSet<ApplicationUser> Users { get; set; }
+
+        int SaveChanges();
     }
 }
---
+++ SegmentSniper.Tests/Services/AuthServices/RegisterUser/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using SegmentSniper.Data;
+using Moq;
+using Microsoft.AspNetCore.Identity;
+using SegmentSniper.Data.Entities;
+
+namespace SegmentSniper.Tests.Services.AuthServices.RegisterUser
+
+{
+    [TestClass]
+    public abstract class TestBase
+    {
+        protected SegmentSniper.Services.AuthServices.RegisterUser Service;
+        protected Mock<ISegmentSniperDbContext> Context;
+        protected Mock<UserManager<ApplicationUser>> UserMgr;
+
+        [TestInitialize]
+        public virtual void Arrange()
+        {
+            UserMgr = new Mock<UserManager<ApplicationUser>>();
+            Context = new Mock<ISegmentSniperDbContext>();
+            Service = new SegmentSniper.Services.AuthServices.RegisterUser(Context.Object, UserMgr.Object);
+        }
+    }
+}
