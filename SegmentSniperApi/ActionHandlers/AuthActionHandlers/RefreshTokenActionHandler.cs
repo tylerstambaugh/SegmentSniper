233cf3b2bc8095dd65d6d8da48529c056e49db4f
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-08-21 07:34:00 -0400

    rounding out auth services

---
+++ SegmentSniperApi/ActionHandlers/AuthActionHandlers/RefreshTokenActionHandler.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace SegmentSniper.Api.ActionHandlers.AuthActionHandlers
+{
+    public class RefreshTokenActionHandler : IRefreshTokenActionHandler
+    {
+        public RefreshTokenActionHandler()
+        {
+
+        }
+
+        public async Task<RefreshTokenRequest.Response> Handle(RefreshTokenRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
