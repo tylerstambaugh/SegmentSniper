575f39cc6c7539fe73ff18eeede10302e9c77896
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-11-16 05:51:32 -0500

    initializing roles - check

--- SegmentSniper.Data/SegmentSniperDbContext.cs
+++ SegmentSniper.Data/SegmentSniperDbContext.cs
@@ -28,35 +28,6 @@ namespace SegmentSniper.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            //Seeding a  'Administrator' role to AspNetRoles table
-            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole { Id = "2c5e174e-3b0e-446f-86af-483d56fd7210", Name = "Administrator", NormalizedName = "ADMINISTRATOR".ToUpper() });
-
-
-            //a hasher to hash the password before seeding the user to the db
-            var hasher = new PasswordHasher<IdentityUser>();
-
-
-            //Seeding the User to AspNetUsers table
-            modelBuilder.Entity<IdentityUser>().HasData(
-                new IdentityUser
-                {
-                    Id = "8e445865-a24d-4543-a6c6-9443d048cdb9", // primary key
-                    UserName = "myuser",
-                    NormalizedUserName = "MYUSER",
-                    PasswordHash = hasher.HashPassword(null, "Pa$$w0rd")
-                }
-            );
-
-
-            //Seeding the relation between our user and role to AspNetUserRoles table
-            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
-                new IdentityUserRole<string>
-                {
-                    RoleId = "2c5e174e-3b0e-446f-86af-483d56fd7210",
-                    UserId = "8e445865-a24d-4543-a6c6-9443d048cdb9"
-                }
-            );
-
             // Override default AspNet Identity table names
             modelBuilder.Entity<ApplicationUser>(entity => { entity.ToTable(name: "Users"); });
             modelBuilder.Entity<IdentityRole>(entity => { entity.ToTable(name: "Roles"); });
---
+++ segment-sniper-react/src/Plan.md
@@ -0,0 +1,7 @@
+implement roles based features
+
+refactor sniping UI
+
+update forgot password
+
+add delete account
