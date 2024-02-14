ffaa93b1a0364abe9db54b9d7fddd3316c408a8e
Tyler Stambaugh <tyler.stambaugh@icloud.com>
2024-02-14 13:59:23 -0500

    styling custom toast

--- segment-sniper-react/src/components/Molecules/Toast/CustomToast.tsx
+++ segment-sniper-react/src/components/Molecules/Toast/CustomToast.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { toast } from "react-hot-toast";
+import styles from "./CustomToast.module.scss";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, Row } from "react-bootstrap";
 
 export type CustomToastProps = {
   message: string;
@@ -18,15 +22,25 @@ export function CustomToast({ message, type }: CustomToastProps) {
     backgroundColor = "gray";
   }
 
-  toast.custom((t) => (
-    <div style={{ color: "white", padding: "10px", backgroundColor }}>
-      {message}
-      <button
-        onClick={() => toast.dismiss(t.id)}
-        style={{ marginLeft: "10px", cursor: "pointer" }}
-      >
-        Close
-      </button>
-    </div>
-  ));
+  toast.custom(
+    (t) => (
+      <div style={{ color: "white", padding: "10px", backgroundColor }}>
+        <Row className="text-end">
+          <Button
+            onClick={() => toast.dismiss(t.id)}
+            className={styles.toastButton}
+            variant="secondary"
+          >
+            <FontAwesomeIcon icon={faClose} />
+          </Button>
+        </Row>
+        <Row>{message}</Row>
+      </div>
+    ),
+    {
+      duration: Infinity,
+      position: "bottom-center",
+      className: styles.toastContainer,
+    }
+  );
 }
