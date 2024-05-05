155570f98af7971e7ae9ae4b6ff4faebf73a148e
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2024-05-05 08:52:30 -0400

    sets up revoke strava token confirmation modal. Need to add validation

--- segment-sniper-react/src/components/Molecules/Profile/StravaToken/RevokeStravaToken.tsx
+++ segment-sniper-react/src/components/Molecules/Profile/StravaToken/RevokeStravaToken.tsx
@@ -2,50 +2,57 @@ import { Row, Col, Button } from "react-bootstrap";
 import useProfileStore from "../../../../stores/useProfileStore";
 import styles from "./RevokeStravaToken.module.scss";
 import { DateTime } from "luxon";
-import useHandleDeleteStravaToken from "../../../../hooks/Api/Profile/Handlers/useHandleDeleteStravaToken";
+import RevokeStravaTokenConfirmationModal from "./RevokeStravaTokenConfirmationModal";
+import { useState } from "react";
 
 const RevokeStravaToken = () => {
   const [profile] = useProfileStore((state) => [state.profileData]);
-  const { handle: revokeStravaToken, isLoading } = useHandleDeleteStravaToken();
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
 
   return (
-    <Row>
-      <Col className="p-0">
-        <div>
-          <p className={`${styles.profileLabel}`}>Strava API refresh token</p>
-          {!profile?.hasStravaToken ? (
-            <p className={`${styles.profileValue} mb-0 pb-0`}>
-              No Refresh Token Set
-            </p>
-          ) : (
-            <div>
+    <>
+      <RevokeStravaTokenConfirmationModal
+        showRevokeStravaTokenConfirmModal={showConfirmationModal}
+        handleCloseModal={() => setShowConfirmationModal(false)}
+      />
+      <Row>
+        <Col className="p-0">
+          <div>
+            <p className={`${styles.profileLabel}`}>Strava API refresh token</p>
+            {!profile?.hasStravaToken ? (
               <p className={`${styles.profileValue} mb-0 pb-0`}>
-                {profile?.stravaRefreshToken}
+                No Refresh Token Set
               </p>
-              <div className="d-flex pt-2">
-                <p className="ps-3 pt-2 small text-muted">
-                  Expires at{" "}
-                  {profile?.stravaTokenExpiresAt !== null &&
-                  profile?.stravaTokenExpiresAt !== undefined
-                    ? profile?.stravaTokenExpiresAt.toLocaleString(
-                        DateTime.DATETIME_FULL
-                      )
-                    : ""}
+            ) : (
+              <div>
+                <p className={`${styles.profileValue} mb-0 pb-0`}>
+                  {profile?.stravaRefreshToken}
                 </p>
-                <Button
-                  variant="third"
-                  className={`${styles.revokeButton} ms-2`}
-                  onClick={revokeStravaToken}
-                >
-                  Revoke
-                </Button>
+                <div className="d-flex pt-2">
+                  <p className="ps-3 pt-2 small text-muted">
+                    Expires at{" "}
+                    {profile?.stravaTokenExpiresAt !== null &&
+                    profile?.stravaTokenExpiresAt !== undefined
+                      ? profile?.stravaTokenExpiresAt.toLocaleString(
+                          DateTime.DATETIME_FULL
+                        )
+                      : ""}
+                  </p>
+                  <Button
+                    variant="third"
+                    className={`${styles.revokeButton} ms-2`}
+                    onClick={() => setShowConfirmationModal(true)}
+                  >
+                    Revoke
+                  </Button>
+                </div>
               </div>
-            </div>
-          )}
-          <hr className={styles.hrCentered} />
-        </div>
-      </Col>
-    </Row>
+            )}
+            <hr className={styles.hrCentered} />
+          </div>
+        </Col>
+      </Row>
+    </>
   );
 };
 
