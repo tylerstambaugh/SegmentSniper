587533775b8bc1486449f8ccc57004ab06705611
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2024-10-14 20:59:13 -0400

    more work on footer

--- segment-sniper-react/src/components/Organisms/Footer/Footer.tsx
+++ segment-sniper-react/src/components/Organisms/Footer/Footer.tsx
@@ -8,38 +8,63 @@ import {
   faLinkedin,
 } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import styles from './footer.module.scss';
+import '../../../App.css';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="footer">
+    <footer
+      className={`${styles.footer} d-flex flex-column justify-content-center align-items-center`}
+    >
       <Container>
         <Row>
-          <Col xs={12} md={4}>
-            <p>Copyright 2023 Tyler Stambaugh</p>
+          <Col className="text-center">
+            <p>Copyright 2024 Tyler Stambaugh</p>
           </Col>
+        </Row>
+        <Row style={{ width: '50%', alignItems: 'center' }}>
           <Col>
             <a href="mailto:segmentsniper@gmail.com" target="_blank">
-              <FontAwesomeIcon icon={faEnvelope} />
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                size="xs"
+                className={`${styles.fa_icon}`}
+              />
             </a>
           </Col>
-
           <Col>
-            <a href="https://github.com/tylerstambaugh">
+            <a
+              href="https://github.com/tylerstambaugh"
+              target="_blank"
+              className={`${styles.fa_icon}`}
+            >
               <FontAwesomeIcon icon={faGithub} />
             </a>
           </Col>
           <Col>
-            <a href="https://www.strava.com/athletes/381648">
+            <a
+              href="https://www.strava.com/athletes/381648"
+              target="_blank"
+              className={`${styles.fa_icon}`}
+            >
               <FontAwesomeIcon icon={faStrava} />
             </a>
           </Col>
           <Col>
-            <a href="https://www.strava.com/athletes/381648">
+            <a
+              href="https://www.instagram.com/tyler_stambaugh/"
+              target="_blank"
+              className={`${styles.fa_icon}`}
+            >
               <FontAwesomeIcon icon={faInstagram} />
             </a>
           </Col>
           <Col>
-            <a href="https://www.linkedin.com/in/tyler-stambaugh-b274a59/">
+            <a
+              href="https://www.linkedin.com/in/tyler-stambaugh-b274a59/"
+              target="_blank"
+              className={`${styles.fa_icon}`}
+            >
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
           </Col>
