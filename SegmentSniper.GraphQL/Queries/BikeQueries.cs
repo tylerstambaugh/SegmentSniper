3fda90d14edca7d7dbe7eeaeea674f3c36bfdcda
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2024-11-30 21:29:26 -0500

    building out gql types for garage

---
+++ SegmentSniper.GraphQL/Queries/BikeQueries.cs
@@ -0,0 +1,16 @@
+﻿using GraphQL.Types;
+using SegmentSniper.GraphQL.Types;
+
+namespace SegmentSniper.GraphQL.Queries
+{
+    public class BikeQueries : ObjectGraphType
+    {
+        public BikeQueries()
+        {
+            //Field<ListGraphType<BikeTypeDef>>(
+            //    "bikes",
+            //    resolve: context => 
+            //    });
+        }
+    }
+}
---
+++ SegmentSniper.Services/Garage/GetBikeById.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegmentSniper.Services.Garage
+{
+    internal class GetBikeById
+    {
+    }
+}
