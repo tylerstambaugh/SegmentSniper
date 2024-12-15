548a43880e3da0327504d88d8f08a1b1df1b8094
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2024-12-14 20:43:49 -0500

    setting up to call graphQl

--- segment-sniper-react/src/components/Molecules/Garage/BikeList/BikeList.tsx
+++ segment-sniper-react/src/components/Molecules/Garage/BikeList/BikeList.tsx
@@ -1,11 +1,61 @@
-import React, { useState } from "react";
-import { Col, Form, Row } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Col, Form, Row } from 'react-bootstrap';
+import { useQuery, gql } from '@apollo/client';
+import { BikeListItem } from './BikeListItem';
 
-export const BikeList = () => {
+type Bike = {
+    bikeId: string;
+    name: string;
+    brandName: string;
+    modelName: string;
+    frameType: string;
+    metersLogged: number;
+};
 
+type GetBikesByUserIdResponse = {
+    Bikes: {
+        byUserId: Bike[];
+    };
+};
+
+const GET_BIKES_BY_USER_ID = gql`
+  query GetBikesByUserId($userId: ID!) {
+    Bikes {
+      byUserId(userId: $userId) {
+        bikeId
+        name
+        brandName
+        modelName
+        frameType
+        metersLogged
+      }
+    }
+  }
+`;
+
+export const BikeList = ({ userId }: { userId: string }) => {
+    const { data, loading, error } = useQuery<GetBikesByUserIdResponse>(GET_BIKES_BY_USER_ID, {
+        variables: { userId },
+    });
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
 
     return (
         <>
+            {data?.Bikes?.byUserId?.map((bike) => (
+                <div key={bike.bikeId}>
+                    <BikeListItem
+                        id={bike.bikeId}
+                        name={bike.name}
+                        brandName={bike.brandName}
+                        modelName={bike.modelName}
+                        frameType={bike.frameType}
+                        distanceInMeters={bike.metersLogged}
+                    />
+                </div>
+            ))}
         </>
-    )
-}
+    );
+};
+
---
+++ segment-sniper-react/src/enums/FrameTypes.ts
@@ -0,0 +1,21 @@
+export enum FrameType {
+  MTB = 'MTB',
+  Corss = 'Cross',
+  Road = 'Road',
+  TimeTrail = 'Time Trail',
+  Gravel = 'Grabel',
+  Ebike = 'E-Bike',
+  Other = 'Other',
+}
+
+// public enum FrameType
+// {
+//     None,
+//     MTB = 1,
+//     Cross = 2,
+//     Road = 3,
+//     TimeTrial = 4,
+//     Gravel = 5,
+//     Ebike = 6,
+//     Other = 7
+// }
