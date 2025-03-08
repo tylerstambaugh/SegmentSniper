d3f13e806738922a2c037d7df34a4e27d42ebf93
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2025-03-07 21:20:06 -0500

    starting on update garage, noodling on update equipment

--- UpdateBikeEquipmentFunction/Function1.cs
+++ UpdateBikeEquipmentFunction/Function1.cs
@@ -11,6 +11,9 @@ namespace UpdateBikeEquipmentFunction
         public void Run([QueueTrigger("segmentsniperdevqueue", Connection = "UpdateBikeEquipmentFunctionQueueConnection")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+
+            //this should run on an interval and tabulate the miles for each piece of equipment on each bike.
+            // each piece of quipment should have the miles calculated as the sum of the miles for the bikeActivities since the install date of the equipment.
         }
     }
 }
