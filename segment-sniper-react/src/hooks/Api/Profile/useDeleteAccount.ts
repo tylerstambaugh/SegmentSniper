5db7901888dc8e0f2b357cd4230ecce0c33e7ba1
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2025-03-21 20:56:51 -0400

    fixed another hook to use updated react-query

--- segment-sniper-react/src/hooks/Api/Profile/useDeleteAccount.ts
+++ segment-sniper-react/src/hooks/Api/Profile/useDeleteAccount.ts
@@ -4,10 +4,11 @@ import useApiConfigStore from '../../../stores/useApiConfigStore';
 import useTokenDataStore from '../../../stores/useTokenStore';
 import useProfileStore from '../../../stores/useProfileStore';
 import useUserStore from '../../../stores/useUserStore';
-import deleteAccount from '../../../services/Api/Profile/deleteAccount';
+import deleteAccount, {
+  DeleteAccountResponse,
+} from '../../../services/Api/Profile/deleteAccount';
 
 export const useDeleteAccount = () => {
-  const { mutateAsync, isLoading, isError, error, data } = useMutation(trigger);
   const apiConfig = useApiConfigStore((state) => state.apiConfig);
   const resetProfileData = useProfileStore((state) => state.resetProfileData);
   const resetUserStore = useUserStore((state) => state.resetUserStore);
@@ -15,20 +16,22 @@ export const useDeleteAccount = () => {
     state.tokenData?.accessToken,
     state.resetTokenDataStore,
   ]);
-  async function trigger() {
-    const contract: ApiContract = {
-      baseUrl: apiConfig!.baseRestApiUrl,
-      token: accessToken!,
-    };
 
-    const response = await deleteAccount(contract);
-    if (response.success) {
-      resetTokendata();
-      resetProfileData();
-      resetUserStore();
-    }
-    return response;
-  }
+  const mutation = useMutation<DeleteAccountResponse, Error>({
+    mutationFn: async () => {
+      const contract: ApiContract = {
+        baseUrl: apiConfig!.baseRestApiUrl,
+        token: accessToken!,
+      };
 
-  return { mutateAsync, isLoading, isError, error, data };
+      const response = await deleteAccount(contract);
+      if (response.success) {
+        resetTokendata();
+        resetProfileData();
+        resetUserStore();
+      }
+      return response;
+    },
+  });
+  return mutation;
 };
