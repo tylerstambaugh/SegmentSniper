86ee4bac24205f75e5cc3823d93b9975ba6aa854
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2025-05-28 20:58:30 -0400

    getting webhook subscription ready

---
+++ SegmentSniperApi/Controllers/WebhookController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Asn1.Cmp;
+using System.Text.Json.Serialization;
+
+namespace SegmentSniper.Api.Controllers
+{
+
+    public record StravaVerifyResponse(
+    [property: JsonPropertyName("hub.challenge")] string HubChallenge);
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Verify(
+      [FromQuery(Name = "hub.challenge")] string challenge,
+      [FromQuery(Name = "hub.mode")] string mode,
+      [FromQuery(Name = "hub.verify_token")] string verifyToken)
+        {
+            // Optional: ensure mode == "subscribe" and verifyToken matches your secret
+
+            return Ok(new StravaVerifyResponse(challenge));
+        }
+    }
+}
--- segment-sniper-react/src/components/Organisms/Admin/AdminMenu/AdminMenu.tsx
+++ segment-sniper-react/src/components/Organisms/Admin/AdminMenu/AdminMenu.tsx
@@ -6,6 +6,14 @@ export default function AdminWidget() {
       <Row>
         <Col>
           <h2>admin menu widget</h2>
+
+          need to put button here to subscribe to strava webhooks.  It should:
+
+          POST https://www.strava.com/api/v3/push_subscriptions \
+          -F client_id=5 \
+          -F client_secret=7b2946535949ae70f015d696d8ac602830ece412 \
+          -F callback_url=https://as-segmentsniper-api-eastus-dev.azurewebsites.net/api/webhook
+          -F verify_token=STRAVA
         </Col>
       </Row>
     </Container>
