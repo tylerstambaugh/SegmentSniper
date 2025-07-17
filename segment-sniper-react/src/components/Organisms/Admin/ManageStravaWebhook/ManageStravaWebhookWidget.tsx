88ef1c8a47bc77490d536c2611721789340b52ff
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2025-07-16 21:42:22 -0400

    leet code sql top 50 exercises

--- segment-sniper-react/src/components/Organisms/Admin/ManageStravaWebhook/ManageStravaWebhookWidget.tsx
+++ segment-sniper-react/src/components/Organisms/Admin/ManageStravaWebhook/ManageStravaWebhookWidget.tsx
@@ -8,6 +8,12 @@ import { DeleteSubscriptionModal } from "../../../Molecules/ManageStravaWebhook/
 import { useGetSubscriptionId } from "../../../../hooks/Api/Admin/StravaWebhook/useGetSubscriptionId";
 
 
+// SELECT s.user_id,
+//     ROUND(ISNULL(AVG(CASE WHEN action = 'confirmed' THEN 1.0 ELSE 0 END), 0), 2) AS confirmation_rate
+// FROM Signups s
+// LEFT JOIN Confirmations c ON s.user_id = c.user_id
+// GROUP BY s.user_id
+
 
 const ManageStravaWebhookWidget = () => {
 
