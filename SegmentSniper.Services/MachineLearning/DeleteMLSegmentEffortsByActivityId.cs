c3a67522f1cac69775688df2f5b91621a026efb9
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2025-07-20 19:20:17 -0400

    handling webhook events is like building another app

---
+++ SegmentSniper.Services/MachineLearning/DeleteMLSegmentEffortsByActivityId.cs
@@ -0,0 +1,27 @@
+﻿using SegmentSniper.Data;
+using SegmentSniper.Services.Interface;
+
+namespace SegmentSniper.Services.MachineLearning
+{
+    public class DeleteMLSegmentEffortsById : IDeleteMLSegmentEffortsById, IExecutableServiceAsync<DeleteMLSegmentEffortsByIdContract, DeleteMLSegmentEffortsByIdContract.Result>
+    {
+        private readonly ISegmentSniperDbContext _segmentSniperDbContext;
+
+        public DeleteMLSegmentEffortsById(ISegmentSniperDbContext segmentSniperDbContext)
+        {
+            _segmentSniperDbContext = segmentSniperDbContext;
+        }
+
+        public async Task<DeleteMLSegmentEffortsByIdContract.Result> ExecuteAsync(DeleteMLSegmentEffortsByIdContract contract)
+        {
+
+            var segmentsToDelete = _segmentSniperDbContext.ML_SegmentEfforts.Where(e => contract.SegmentEffortIds.Contains(e.SegmentEffortId);
+
+            _segmentSniperDbContext.ML_SegmentEfforts.RemoveRange(segmentsToDelete);
+
+            _segmentSniperDbContext.SaveChanges();
+
+            return new DeleteMLSegmentEffortsByIdContract.Result();
+        }       
+    }
+}
