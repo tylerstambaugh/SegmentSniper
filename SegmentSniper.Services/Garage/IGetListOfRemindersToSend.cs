c9539e5fda65342dedb655f217a2a407729717a7
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2025-08-09 21:41:13 -0400

    setting up srvr less FUNction

---
+++ SegmentSniper.Services/Garage/IGetListOfRemindersToSend.cs
@@ -0,0 +1,15 @@
+﻿namespace SegmentSniper.Services.Garage
+{
+    public interface IGetListOfRemindersToSend
+    {
+        Task<List<ReminderModel>> ExecuteAsync(GetListOfRemindersToSendContract contract);
+    }
+
+    public class ReminderModel
+    {
+    }
+
+    public class GetListOfRemindersToSendContract
+    {
+    }
+}
