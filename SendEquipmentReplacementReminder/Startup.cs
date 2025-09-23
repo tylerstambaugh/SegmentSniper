5f07a2b4b082e6e5715a03da2cb17dc2532666e3
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2025-09-22 21:21:57 -0400

    setting up function app startup

---
+++ SendEquipmentReplacementReminder/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SegmentSniper.Data;
+
+[assembly: FunctionsStartup(typeof(SendEquipmentReplacementReminder.Startup))]
+namespace SendEquipmentReplacementReminder
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var conn = Environment.GetEnvironmentVariable("SqlConnectionString");
+
+            // Scoped lifetime is correct for EF Core
+            builder.Services.AddDbContext<SegmentSniperDbContext>(options =>
+                options.UseSqlServer(conn));
+
+            // Register other services or repositories here
+            
+        }
+    }
+}
