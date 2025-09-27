b6ab724fbb0cf5aa561a3d19b834099e60072fae
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2025-09-26 20:35:55 -0400

    update equipment for totalMiles

--- SegmentSniper.Data/Migrations/20250926135506_addsMilesAtInstallToEquipment.Designer.cs
+++ SegmentSniper.Data/Migrations/20250926135506_addsMilesAtInstallToEquipment.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SegmentSniper.Data;
 
@@ -11,9 +12,11 @@ using SegmentSniper.Data;
 namespace SegmentSniper.Data.Migrations
 {
     [DbContext(typeof(SegmentSniperDbContext))]
-    partial class SegmentSniperDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250926135506_addsMilesAtInstallToEquipment")]
+    partial class addsMilesAtInstallToEquipment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
