048a4c89d93445a6c1d942da9887be946545b8da
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2025-09-30 22:20:42 -0400

    update Equipment to include reminder date

--- SegmentSniper.GraphQL/Types/EquipmentTypeDef.cs
+++ SegmentSniper.GraphQL/Types/EquipmentTypeDef.cs
@@ -17,6 +17,8 @@ namespace SegmentSniper.GraphQL.Types
             Field<DateTimeGraphType>("installDate").Description("The date the equipment was installed");
             Field<DateTimeGraphType>("updatedDate").Description("The date the equipment was last updated");
             Field<DateTimeGraphType>("retiredDate").Description("The date the equipment was retired");
+            Field<DateTimeGraphType>("reminderDate").Description("The date you want to be reminded to replace the equipment");
+            Field<TimeSpanSecondsGraphType>("reminderDuration").Description("The amount of time until you want to be remined to replace the equipment");
             Field<DecimalGraphType>("totalMiles").Description("Total miles on the equipment");
             Field<DecimalGraphType>("milesAtInstall").Description("The miles on the equipment when it was added to this bike");
             Field<IntGraphType>("replaceAtMiles").Description("The sum total of miles that the equipment should be replaced");
@@ -36,6 +38,8 @@ namespace SegmentSniper.GraphQL.Types
             Field(e => e.InstallDate).Description("The installation date of the equipment.");
             Field(e => e.UpdatedDate).Description("The installation date of the updated.");
             Field(e => e.RetiredDate).Description("The date the equipment was retired");
+            Field(e => e.ReminderDate).Description("The date you want to be reminded to replace the equipment");
+            Field(e => e.ReminderDuration).Description("The amount of time until you want to be remined to replace the equipment");
             Field(e => e.TotalMiles).Description("The miles logged on the equipment");
             Field(e => e.MilesAtInstall).Description("The miles on the equipment when it was added to this bike");
             Field(e => e.ReplaceAtMiles).Description("The sum total of miles that the equipment should be replaced");
--- SegmentSniper.Models/Garage/EquipmentModel.cs
+++ SegmentSniper.Models/Garage/EquipmentModel.cs
@@ -15,6 +15,8 @@ namespace SegmentSniper.Models.Garage
         public DateTime? InstallDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
         public DateTime? RetiredDate { get; set; }
+        public DateTime? ReminderDate { get; set; }
+        public TimeSpan? ReminderDuration { get; set; }
         public decimal Price { get; set; }
         public int ReplaceAtMiles { get; set; }
         public int MilesUntilReplaceReminder { get; set; }
