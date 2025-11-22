8ab85fa49aeb1b16fb80428e3e86ba57ad3d4421
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2025-11-22 18:36:28 -0500

    add baseEntity to all entities

---
+++ SegmentSniper.Data/Migrations/20251122232959_addBaseEntityToEntities.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SegmentSniper.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addBaseEntityToEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "StravaWebhookSubscription",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "StravaWebhookSubscription",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "SegmentPredictionRegressionMetrics",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "SegmentPredictionRegressionMetrics",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "SegmentPredictionRegressionMetrics",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "ML_SegmentPredictionModels",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "ML_SegmentEfforts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "ML_SegmentEfforts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "ML_SegmentEfforts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Equipment",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "Equipment",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Bikes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "Bikes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Bikes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "BikeActivities",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "BikeActivities",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "BikeActivities",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "StravaWebhookSubscription");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "StravaWebhookSubscription");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "SegmentPredictionRegressionMetrics");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "SegmentPredictionRegressionMetrics");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "SegmentPredictionRegressionMetrics");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "ML_SegmentPredictionModels");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "ML_SegmentEfforts");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "ML_SegmentEfforts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "ML_SegmentEfforts");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Equipment");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "Equipment");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Bikes");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "Bikes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Bikes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "BikeActivities");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "BikeActivities");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "BikeActivities");
+        }
+    }
+}
