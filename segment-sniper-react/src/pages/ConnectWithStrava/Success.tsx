741f899a356d3bb27daa7abf2c6396e7fc81cf4e
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2025-12-05 07:34:19 -0500

    working through signup workflows

--- segment-sniper-react/src/pages/ConnectWithStrava/Success.tsx
+++ segment-sniper-react/src/pages/ConnectWithStrava/Success.tsx
@@ -1,19 +1,40 @@
-import { Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { AppRoutes } from "../../enums/AppRoutes";
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { AppRoutes } from '../../enums/AppRoutes';
+import { useUser } from '@clerk/clerk-react';
+import { useEffect } from 'react';
 
 function ConnectWithStravaSuccess() {
+  const { user, isLoaded } = useUser();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isLoaded) {
+      user?.reload();
+    }
+  }, []);
   return (
-    <Container className="d-flex flex-column align-items-center justify-content-center pt-5">
-      <Row className="text-center ">
-        <Col className="mx-auto">
-          <p>Authorization was successful.</p>
+    <Container className="d-flex flex-column justify-content-center">
+      <Row className="d-flex justify-content-center pt-3 ">
+        <Col xl={6} lg={8} md={10} sm={12} xs={12}>
+          <Card className="shadow">
+            <Card.Body className="mx-3">
+              <div className="mb-2 text-center">
+                <h2 className="fw-bold mb-2 ">Authorization successful</h2>
+                <Row>
+                  <Col className="d-flex p-2 mb-2 justify-content-center">
+                    <Button
+                      onClick={() => navigate(`/${AppRoutes.Dashboard}`)}
+                      variant="primary"
+                      className="mt-3"
+                    >
+                      Main Menu
+                    </Button>
+                  </Col>
+                </Row>
+              </div>
+            </Card.Body>
+          </Card>
         </Col>
-      </Row>
-      <Row>
-        <Link to={`/${AppRoutes.Dashboard}`} className="rounded-button">
-          Take me to the app
-        </Link>
       </Row>
     </Container>
   );
--- segment-sniper-react/src/pages/Pricing/BillingSuccess.tsx
+++ segment-sniper-react/src/pages/Pricing/BillingSuccess.tsx
@@ -1,28 +1,47 @@
 import { useEffect } from 'react';
-import { useUser } from '@clerk/clerk-react';
-import { Button } from 'react-bootstrap';
+import { useSession, useUser } from '@clerk/clerk-react';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import { AppRoutes } from '../../enums/AppRoutes';
 import { useNavigate } from 'react-router';
 
 export default function BillingSuccess() {
   const { user, isLoaded } = useUser();
+  const { session } = useSession();
   const navigate = useNavigate();
   useEffect(() => {
-    if (isLoaded) {
-      user?.reload();
-    }
+    if (!isLoaded) return;
+
+    (async () => {
+      console.log('Reloading user and session after billing success');
+      await user?.reload();
+      await session?.reload();
+    })();
   }, []);
 
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Subscription Updated 🚀</h1>
-      <Button
-        onClick={() => navigate(`/${AppRoutes.Dashboard}`)}
-        variant="primary"
-        className="mt-3"
-      >
-        Main Menu
-      </Button>
-    </div>
+    <Container className="d-flex flex-column justify-content-center">
+      <Row className="d-flex justify-content-center pt-3 ">
+        <Col xl={6} lg={8} md={10} sm={12} xs={12}>
+          <Card className="shadow">
+            <Card.Body className="mx-3">
+              <div className="mb-2 text-center">
+                <h2 className="fw-bold mb-2 ">Subscription Updated</h2>
+                <Row>
+                  <Col className="d-flex p-2 mb-2 justify-content-center">
+                    <Button
+                      onClick={() => navigate(`/${AppRoutes.Dashboard}`)}
+                      variant="primary"
+                      className="mt-3"
+                    >
+                      Main Menu
+                    </Button>
+                  </Col>
+                </Row>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
